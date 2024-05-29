faad7197a2f7eef0ae35ea8f120878966de39bea
unknown <stheesien@gmail.com>
2024-05-29 23:08:19 +0800

    Created BookService class and method to retrieve all books

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}

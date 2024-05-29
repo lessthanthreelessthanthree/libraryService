0e91e42c3e10db525ebbae5ca521b6197005075d
unknown <stheesien@gmail.com>
2024-05-30 00:35:26 +0800

    Added Borrower Service class with fetch all borrowers method

---
+++ src/main/java/com/example/demo/service/BorrowerService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.BorrowerDto;
+import com.example.demo.model.Borrower;
+import com.example.demo.repository.BorrowerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BorrowerService {
+
+    private BorrowerRepository borrowerRepository;
+
+    @Autowired
+    public BorrowerService(BorrowerRepository borrowerRepository) {
+        this.borrowerRepository = borrowerRepository;
+    }
+
+    public List<BorrowerDto> getAllBorrowers() {
+        return borrowerRepository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private BorrowerDto convertToDTO(Borrower borrower) {
+        return new BorrowerDto(borrower.getId(), borrower.getName(), borrower.getEmail());
+    }
+}
